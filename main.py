41058659c96348a72cac73425173f9737dc3ddff
MicheleCorradi <michelecorradik@gmail.com>
2021-05-25 16:09:21 +0200

    Ho creato la funzione che calcola l'età

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ myimage = Label(image=photo)
 myimage.grid(row=0,column=1)
 
 
+def calculateAge():
+    today = date.today()
+    birthdate = date(int(yearEntry.get(),int(monthEntry.get()),int(dayEntry.get())))
+    age = today.year - birthdate.year - ((today.month,today.day) < (birthdate.month, birthdate.day))
+    Label(text=f"{nameValue.get()} your age is {age}").grid(row=6, column=1)
