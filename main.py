7209df977957f04cf0795714aadbb41aa9e6de8f
MicheleCorradi <michelecorradik@gmail.com>
2021-05-25 15:50:33 +0200

    Creo la finestra principale e la label per l'immagine

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+from tkinter import *
+from datetime import date 
+
+root = Tk()
+root.geometry("700x500")
+root.title("Age Calculator")
+
+photo = PhotoImage(file="filename.png")
+myimage = Label(image=photo)
+myimage.grid(row=0,column=1)
+
+
